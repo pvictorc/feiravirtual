e965f90066fdb0e25fe4d3f69521258c12c3125f
RoneyCh <roneychristian159@gmail.com>
2021-12-06 20:42:16 -0300

    Criação da tela produtos

--- src/pages/Categorias/index.js
+++ src/pages/Categorias/index.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native'
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { useNavigation   } from '@react-navigation/native';
+import Detail from '../Detail';
+import Produtos from '../../components/Produtos';
 
 export default function Categorias() {
+
+    const navigation = useNavigation();
+
     return (
-        <View>
-            <Text>Categorias</Text>
-        </View>
+       <>
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={{ flexDirection: 'row', justifyContent:'space-around'}}>
+                <Produtos onClick={() => alert('Clicou')}  />
+                <Produtos onClick={() => alert('Clicou')} />
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent:'space-around'}}>
+                <Produtos/>
+                <Produtos/>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent:'space-around'}}>
+                <Produtos/>
+                <Produtos/>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent:'space-around'}}>
+                <Produtos/>
+                <Produtos/>
+            </View>
+        </ScrollView>
+       </>
     )
 }
