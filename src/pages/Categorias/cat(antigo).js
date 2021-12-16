9fbf54dc396b6e8a26e09c12a7e71d51e3f761da
RoneyCh <roneychristian159@gmail.com>
2021-12-16 20:56:50 -0300

    listar categorias from api

---
+++ src/pages/Categorias/cat(antigo).js
@@ -0,0 +1,86 @@
+/*import React from 'react'
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native'
+import {  TouchableOpacity } from 'react-native';
+import { useNavigation   } from '@react-navigation/native';
+// import Detail from '../Detail';
+import Produtos from '../../components/Produtos';
+import { render } from 'react-dom';
+
+const fetch = require('node-fetch');
+const url = "https://mercadosocial.socialtec.net.br/api/categorias/";
+let arrcategorias = [];
+
+export default class Categorias extends React.Component {
+    state = {
+        data: {}
+    }
+    
+    getJsonData = () => {
+        fetch(url,{method: 'GET'})
+        .then( (response)=> response.json() )
+        .then((responseJson) => {
+            console.log(responseJson);
+            for (let i=0;i<responseJson.length;i++){
+                console.log (responseJson[i]);
+                arrcategorias.push(responseJson[i]);
+            }
+            this.setState({
+                data: responseJson
+            })
+        })
+        .catch((error) =>{
+            console.error(error)
+        });
+    }
+    
+    componentDidMount = () => {
+        this.getJsonData()
+    }
+
+    render(){
+
+        return (
+            <ScrollView showsVerticalScrollIndicator={false}>  
+                <View style={styles.catContainer}>
+                {arrcategorias.map(item => {
+                    return (                             
+                        <View key={item.id}>
+                        <TouchableOpacity style={styles.container}>
+                            <Image style={{minWidth: 150, minHeight: 150}} source={{uri: item.imagem}}/>
+                            <Text  style={styles.produto}>{item.nome}</Text>           
+                        </TouchableOpacity> 
+                        </View>                     
+                    );
+                })} 
+                </View>
+            </ScrollView>
+        )
+    }
+
+}
+    // estilizando categorias
+    const styles = StyleSheet.create({
+        catContainer:{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            flexWrap: 'wrap',
+
+        },
+        container:{
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: 175,
+            height: 175,
+            marginHorizontal: '2%',
+            marginVertical: '10%',
+
+        },
+        produto: {       
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: 20,
+        }
+    });
+
+
+*/
--- src/pages/Categorias/index.js
+++ src/pages/Categorias/index.js
@@ -1,85 +1,71 @@
-import React from 'react'
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native'
-import {  TouchableOpacity } from 'react-native';
-import { useNavigation   } from '@react-navigation/native';
-// import Detail from '../Detail';
-import Produtos from '../../components/Produtos';
-import { render } from 'react-dom';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, ActivityIndicator} from 'react-native';
+import { Image, StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
-const fetch = require('node-fetch');
-const url = "https://mercadosocial.socialtec.net.br/api/categorias/";
-let arrcategorias = [];
+export default function cat() {
 
-export default class Categorias extends React.Component {
-    state = {
-        data: {}
-    }
-    
-    getJsonData = () => {
-        fetch(url,{method: 'GET'}).
-        then( (response)=> response.json() )
-        .then((responseJson) => {
-            console.log(responseJson);
-            for (let i=0;i<responseJson.length;i++){
-                console.log (responseJson[i]);
-                arrcategorias.push(responseJson[i]);
-            }
-            this.setState({
-                data: responseJson
-            })
-        })
-        .catch((error) =>{
-            console.error(error)
-        });
-    }
-    
-    componentDidMount = () => {
-        this.getJsonData()
-    }
+    const[carregando, setCarregando] = useState(true);
+    const[dados, setDados] = useState([]);
 
-    render(){
+    useEffect(
+        ()=>{
+            fetch('https://mercadosocial.socialtec.net.br/api/categorias/', {method: 'GET'})
+            .then( (response)=> response.json() )
+            .then((responseJson)=> setDados(responseJson))
+            .catch(()=>(alert('Erro')))
+            .finally(()=>setCarregando(false))
+        },[]
+    )
 
-        return (
-            <ScrollView showsVerticalScrollIndicator={false}>  
-                <View style={styles.catContainer}>
-                {arrcategorias.map(item => {
-                    return (                             
-                        <View key={item.id}>
+    return (
+        <ScrollView showsVerticalScrollIndicator={false}> 
+            {                
+                carregando ? <ActivityIndicator /> : (      
+                        
+                    <FlatList
+                        numColumns={2}
+                        data={dados}
+                        keyExtractor={({id}, index) => id} 
+                        renderItem={({item}) =>(
+                        <View >                 
                         <TouchableOpacity style={styles.container}>
-                            <Image style={{minWidth: 150, minHeight: 150}} source={{uri: item.imagem}}/>
-                            <Text  style={styles.produto}>{item.nome}</Text>           
-                        </TouchableOpacity> 
-                        </View>                     
-                    );
-                })} 
-                </View>
-            </ScrollView>
-        )
-    }
+                             <Image style={{minWidth: 150, minHeight: 150}} source={{uri: item.imagem}}/>
+                             <Text  style={styles.produto}>{item.nome}</Text>          
+                         </TouchableOpacity>     
+                         </View>                                         
+                        )}
+                    />
+                       
+                )               
+            }
+            
 
+        </ScrollView>
+    )
 }
-    // estilizando categorias
-    const styles = StyleSheet.create({
-        catContainer:{
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            flexWrap: 'wrap',
 
-        },
-        container:{
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: 175,
-            height: 175,
-            marginHorizontal: '2%',
-            marginVertical: '10%',
+  // estilizando categorias
+  const styles = StyleSheet.create({
+    catContainer:{
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        flexWrap: 'wrap',
 
-        },
-        produto: {       
-            justifyContent: 'center',
-            alignItems: 'center',
-            fontSize: 20,
-        }
-    });
+    },
+    container:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 175,
+        height: 175,
+        marginHorizontal: '2%',
+        marginVertical: '10%',
 
-
+    },
+    produto: {       
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 20,
+    }
+});
